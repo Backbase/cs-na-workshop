ff7638e88bdebc04c9523215e661870d34650ba0
John Coombs <johnc@backbase.com>
2022-04-21 12:16:16 -0400

    delete target

--- tools/generators/openapi/target/generated-sources/openapi/variables.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const NGRX_PROMOTIONS_BASE_PATH = new InjectionToken<string>('NGRX_PROMOTIONS_BASE_PATH');
-export const COLLECTION_FORMATS = {
-  csv: ',',
-  tsv: '   ',
-  ssv: ' ',
-  pipes: '|',
-};
