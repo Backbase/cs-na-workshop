696b23dd4abab78fcb379b3d8872f0720b444916
John Coombs <johnc@backbase.com>
2022-04-20 23:35:16 -0400

    npm run format

--- libs/retail/util/promotions/src/lib/variables.ts
+++ libs/retail/util/promotions/src/lib/variables.ts
@@ -2,8 +2,8 @@ import { InjectionToken } from '@angular/core';
 
 export const NGRX_PROMOTIONS_BASE_PATH = new InjectionToken<string>('NGRX_PROMOTIONS_BASE_PATH');
 export const COLLECTION_FORMATS = {
-    'csv': ',',
-    'tsv': '   ',
-    'ssv': ' ',
-    'pipes': '|'
-}
+  csv: ',',
+  tsv: '   ',
+  ssv: ' ',
+  pipes: '|',
+};
