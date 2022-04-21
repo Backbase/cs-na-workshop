99be77962f1a47acd67a8ab0b087e1b68b0f7176
John Coombs <johnc@backbase.com>
2022-04-21 10:15:00 -0400

    Update mocks.service.ts

--- libs/marketing-journey/src/lib/services/mocks.service.ts
+++ libs/marketing-journey/src/lib/services/mocks.service.ts
@@ -2,25 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { ProfileHttpService } from '@backbase/retail/util/promotions';
 
 //TODO: Remove after openapi is ready
 
 @Injectable()
 export class MockHttpService {
-  constructor(private http: HttpClient, private profileHttp: ProfileHttpService) {}
+  constructor(private http: HttpClient) {}
 
   submitPromotionForm(user: any): Observable<any> {
-    return this.http.post('/promotions/submit', user).pipe(map((res: any) => res));
+    return this.http.post('/promotions/submit', user);
   }
 
   getUserProfile(): Observable<any> {
-    return this.profileHttp.getUserProfile().pipe(map((res: any) => res));
-//    return this.http.get('/users/me/profile').pipe(map((res: any) => res));
+    return this.http.get('/users/me/profile');
   }
 
   getUserPromotions(): Observable<any> {
-    return this.profileHttp.getUserPromotions().pipe(map((res: any) => res));
-//    return this.http.get('/users/me/promotion').pipe(map((res: any) => res));
+    return this.http.get('/users/me/promotion');
   }
 }
