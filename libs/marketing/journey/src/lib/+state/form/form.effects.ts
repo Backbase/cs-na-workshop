cd754c6ed5c5ec0d17b7f3cf8739f32a3f76d528
jamelgrooms <jamel@backbase.com>
2022-04-26 15:51:01 -0400

    Exercise 2

--- libs/marketing/journey/src/lib/+state/form/form.effects.ts
+++ libs/marketing/journey/src/lib/+state/form/form.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { createEffect, Actions, ofType } from '@ngrx/effects';
 import { ProfileHttpService } from '@backbase/retail/util/promotions';
-import { map, withLatestFrom, switchMap, take } from 'rxjs/operators';
+import { map, withLatestFrom, switchMap, take, mergeMap } from 'rxjs/operators';
 
 import * as FormActions from './form.actions';
 import * as FormSelectors from './form.selectors';
@@ -14,19 +14,14 @@ export class FormEffects {
   getUserProfile$ = createEffect(() =>
     this.actions$.pipe(
       ofType(FormActions.getUserProfile),
-      withLatestFrom(this.store$.select(FormSelectors.isUserProfileLoaded)),
-      switchMap(([action, isUserProfileLoaded]: [any, boolean]) => {
-        return isUserProfileLoaded
-          ? this.store$.select(FormSelectors.getUserProfile).pipe(take(1))
-          : this.profileHttp.getUserProfile();
-      }),
-      map((profile) =>
-        FormActions.loadUserProfileSuccess({
+      mergeMap((action) => this.profileHttp.getUserProfile()
+      .pipe(
+        map(profile => FormActions.loadUserProfileSuccess({
           profile: {
             ...profile,
           },
-        }),
-      ),
+        })),
+      ))
     ),
   );
 
